0bdb5dcf8b61a77c8a3abe28a1901a2f2b495d37
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2023-02-02 14:47:01 -0600

    Re-enable altitude

--- client/js/draw.js
+++ client/js/draw.js
@@ -11,7 +11,7 @@ export function drawWorld() {
 
     const textCtx = calibrateTextCanvas();
     drawHighscore(textCtx);
-    // drawAltitude(textCtx);
+    drawAltitude(textCtx);
     drawServerStatus(textCtx);
 }
 
